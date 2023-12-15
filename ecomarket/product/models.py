1225cf99eb00182d5954e0ca9a0deb53b8d43cbf
sooronson <sooronson@gmail.com>
2023-12-15 20:25:08 +0600

    Done: Skeleton of project and Product's model

---
+++ ecomarket/product/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    parent = models.ForeignKey(
+        "self", blank=True, null=True,
+        related_name="child", on_delete=models.SET_NULL
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"{self.parent} --> {self.title}" if self.parent else self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to="media/", blank=True, null=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_image")
+
+    def __str__(self):
+        return self.product.title
+
